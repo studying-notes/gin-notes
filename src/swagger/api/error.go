aa48bf6c5ab7400c041b9ad8e6d5b250a1faceda
Rustle Karl <fu.jiawei@outlook.com>
2022-02-11 16:39:22 +0800

    migrated

--- src/swagger/api/error.go
+++
@@ -1,8 +0,0 @@
-package api
-
-// 错误模型
-
-type HTTPError struct {
-	Code    int    `json:"code" example:"400"`
-	Message string `json:"message" example:"status bad request"`
-}
--- src/swagger/api/request/request.go
+++
@@ -1,7 +0,0 @@
-package request
-
-// 请求模型
-
-type AddAccount struct {
-	Name string `json:"name" example:"account"`
-}
